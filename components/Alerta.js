5c9bfb818ed7724970ff95ba6bc73ad0269def43
syncure <richvargast@gmail.com>
2021-05-28 19:28:51 -0500

    version 1

---
+++ components/Alerta.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import authContext from '../context/auth/authContext';
+import appContext from '../context/app/appContext';
+
+const Alerta = () => {
+
+    // Extraer mensaje de error para usuarios
+    const AuthContext = useContext(authContext);
+    const { mensaje } = AuthContext;
+
+    // Extraer mensaje de error para archivos
+    const AppContext = useContext(appContext);
+    const { mensaje_archivo } = AppContext;
+
+    return ( 
+        
+            <div className="bg-red-500 py-2 w-full my-3 my-3 max-w-lg text-center text-white mx-auto">
+                {mensaje || mensaje_archivo}
+            </div>                        
+           
+     );
+}
+ 
+export default Alerta;
